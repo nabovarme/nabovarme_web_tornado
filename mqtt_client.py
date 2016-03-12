38b3641f9015795a010d23cce3a6303f161e0bde
sloev <johannes.valbjorn@gmail.com>
2016-03-12 23:03:38 +0100

    add logging

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,21 +1,15 @@
 import paho.mqtt.client as mqtt
 import rethinkdb as r
 import urlparse
+from logging import getLogger
+from helpers.log import get_logger
 
-from pprint import pprint
-#topic: /sample/v1/6185544/1457737320 payload: heap=15592&t1=67.46 C&t2=26.87 C&tdif=40.59 K&flow1=14 l/h&effect1=0.6 kW&hr=71343 h&v1=2745.12 m3&e1=74536 kWh&
+logger = get_logger("mqtt_client")
 
 nabo_channels = [
         "/sample/v1/#",
-        "/status/v1/#",
-        "/uptime/v1/#"
         ]
 r.connect("0.0.0.0", 32768).repl()
-
-sample_table = ""
-
-r.connect("0.0.0.0", 32768).repl()
-
 r_table = r.db("nabovarme").table("samples")
 
 def on_connect(client, userdata, flags, rc):
@@ -23,17 +17,21 @@ def on_connect(client, userdata, flags, rc):
     [client.subscribe(channel) for channel in nabo_channels]
 
 def on_message(client, userdata, msg):
-    #print "topic:", msg.topic, "payload:", msg.payload
-    payload = urlparse.parse_qs(urlparse.urlsplit(msg.payload).path)
-    payload = {key:value for key,value in ((key,dict(zip(["value", "unit"], values[0].split(" ")))) for key, values in payload.iteritems())}
-    payload.pop("heap")
-    doc = dict(zip(["meter_id", "unix_time"],[int(val) for val in msg.topic.split("/")[3:]]))
-    doc['unix_time'] = r.epoch_time(doc["unix_time"])
-    doc["payload"] = payload
-    if payload:
-        pprint(r_table.insert(doc).run())
-    else:
-        print "not inserting", payload
+    try:
+        payload = urlparse.parse_qs(urlparse.urlsplit(msg.payload).path)
+        payload = {key:value for key,value in ((key,dict(zip(["value", "unit"], values[0].split(" ")))) for key, values in payload.iteritems())}
+        payload.pop("heap", None)
+        doc = dict(zip(["meter_id", "unix_time"],[int(val) for val in msg.topic.split("/")[3:]]))
+        doc['unix_time'] = r.epoch_time(doc["unix_time"])
+        doc["payload"] = payload
+        if payload:
+           res = r_table.insert(doc).run()
+           if not res["inserted"] == 1:
+               msg = "not inserted! topic:{} payload:{}".format(msg.topic, msg.payload)
+               raise ValueError(msg)
+    except Exception:
+        logger.exception(msg.topic)
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message

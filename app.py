54a047d0980ef182688de7ac7b8e797fb9e9b89a
sloev <johannes.valbjorn@gmail.com>
2016-03-13 00:02:16 +0100

    add event-stream

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import tornado.web
 from tornado.web import asynchronous
 import time
 class MainHandler(tornado.web.RequestHandler):
+    def initialise(self, source):
+        self.set_header('content-type', 'text/event-stream')
+        self.set_header('cache-control', 'no-cache')
     @asynchronous
     def get(self):
         for i in range(10):
@@ -13,10 +16,32 @@ class MainHandler(tornado.web.RequestHandler):
             self.write(str(i))
             self.flush()
         self.finish()
+class IndexHandler(tornado.web.RequestHandler):
+    s = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script>
+            var source = new EventSource("/stream");
+                source.onmessage = function(event) {
+                console.log(event)
+                        document.getElementById("output").innerHTML += event.data + "<br/>"
+                            }
+                                </script>
+                                </head>
+                                <body>
+                                    <h1>Output</h1>
+                                        <div id="output"></div>
+                                        </body>
+                                        </html>
+    """
+    def get(self):
+        self.finish(self.s)
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/stream", MainHandler),
+        (r"/", IndexHandler),
         ])
 
 if __name__ == "__main__":
